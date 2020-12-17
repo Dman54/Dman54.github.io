9dc29bbb301bbe285e83318d63258ba0f470d30b
dman54 <dr.d-man54@yandex.ru>
2020-12-17 21:44:35 +0700

    change projects description

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+function setSkills(skillsUl) {
+  var skills = skillsUl.querySelectorAll("a");
+  var size = 70*1.5;
+  var R = (size/2)/Math.tan(Math.PI/skills.length)/Math.cos(Math.PI/skills.length);
+  skillsUl.style.height = 2*R + 70 + "px";
+  skillsUl.style.width = 2*R + 70 + "px";
+  // var r = R*Math.cos(Math.PI/skills.length);
+  Array.from(skills).forEach((element, index) => {
+    element.style.left = R + R*Math.cos(2*Math.PI*index/skills.length) + "px";
+    element.style.top = R + R*Math.sin(2*Math.PI*index/skills.length) + "px";
+    element.addEventListener("click", function(e) {
+      e.preventDefault();
+      element.classList.add("active");
+      setTimeout(() => {
+        window.location.href = element.getAttribute("href");
+      }, 800);
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
   if (canUseWebP()) document.body.classList.add("webp");
   else document.body.classList.add("no-webp");
-  // Array.from(document.getElementsByTagName("li")).forEach((element, index) => {
-  //   element.innerHTML = index;
-  // });
+});
+
+window.addEventListener("load", function (e) {
+  var skillsUl = document.querySelector(".skills");
+  if (skillsUl) setSkills(skillsUl);
 });
 
 function canUseWebP() {
